7123357c203f1d25a687c28db2502ae99709bf53
ehilding <erik.hildingsson@gmail.com>
2021-10-07 15:45:30 +0200

    Added navigation functionality to buttonOne and buttonTwo

--- app/src/main/java/com/ehilding/hangman_ehilding/MainActivity.java
+++ app/src/main/java/com/ehilding/hangman_ehilding/MainActivity.java
@@ -2,13 +2,38 @@ package com.ehilding.hangman_ehilding;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonOne;
+    Button buttonTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonOne = (Button) findViewById(R.id.button_one);
+        buttonTwo = (Button) findViewById(R.id.button_two);
+
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
