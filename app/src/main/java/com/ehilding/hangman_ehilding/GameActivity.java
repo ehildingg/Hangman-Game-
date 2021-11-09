b4e04ffc224ce13022beba2cb05bd86a547d75cc
ehilding <erik.hildingsson@gmail.com>
2021-11-09 10:06:50 +0100

    Typ fungerande newGame

--- app/src/main/java/com/ehilding/hangman_ehilding/GameActivity.java
+++ app/src/main/java/com/ehilding/hangman_ehilding/GameActivity.java
@@ -3,16 +3,109 @@ package com.ehilding.hangman_ehilding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+    ImageView hangMan;
+    TextView textAttempts, textAttemptsTicker, textWordToFind;
+    EditText userCharInput;
+    Button buttonGuess;
+    Button buttonNewGame;
+    GameEngine game = GameEngine.getInstance();
+    public char userInput;
+    public static String wordToFind;
+    public static String wordHint;
+    public int nbErrors = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        GameEngine game = GameEngine.getInstance();
-        game.promptSysout();
+
+        initializeScreen();
+        setupOnClickers();
+
+    }
+
+    private void setupOnClickers() {
+
+        buttonNewGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonNewGame.setVisibility(View.GONE);
+                newGame();
+            }
+        });
+
+        buttonGuess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void initializeScreen() {
+        textAttempts = findViewById(R.id.text_attempts);
+        textAttemptsTicker = findViewById(R.id.text_attempts_ticker);
+        textWordToFind = findViewById(R.id.word_to_find);
+        userCharInput = findViewById(R.id.editxt_enter_char);
+        buttonGuess = findViewById(R.id.btn_guess);
+        hangMan = findViewById(R.id.img_hang_one);
+        buttonNewGame = findViewById(R.id.btn_new_game);
+
+        updateUi();
+
+    }
+
+    private void updateUi() {
+        switch (nbErrors) {
+            case 0: hangMan.setImageResource(0);
+            break;
+            case 1: hangMan.setImageResource(R.drawable.game0);
+            break;
+            case 2: hangMan.setImageResource(R.drawable.game1);
+            break;
+            case 3: hangMan.setImageResource(R.drawable.game2);
+            break;
+            case 4: hangMan.setImageResource(R.drawable.game3);
+            break;
+            case 5: hangMan.setImageResource(R.drawable.game4);
+            break;
+            case 6: hangMan.setImageResource(R.drawable.game5);
+            break;
+            case 7: hangMan.setImageResource(R.drawable.game6);
+            break;
+            case 8: hangMan.setImageResource(R.drawable.game7);
+        }
+    }
+
+    public void newGame() {
+
+        wordToFind = game.nextWordToFind();
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < wordToFind.length(); i++) {
+            builder.append('-');
+        }
+        wordHint = builder.toString();
+        textWordToFind.setText(wordHint);
+
+        playsGame();
+
+    }
+
+    public void playsGame() {
+
+
 
     }
 }
--- app/src/main/java/com/ehilding/hangman_ehilding/GameEngine.java
+++ app/src/main/java/com/ehilding/hangman_ehilding/GameEngine.java
@@ -2,8 +2,26 @@ package com.ehilding.hangman_ehilding;
 
 import android.widget.Toast;
 
+import org.xml.sax.helpers.AttributesImpl;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class GameEngine {
     private static GameEngine hangmanGame = null;
+
+    private static final String[] WORDS = {
+            "TACO", "BUENO", "ADIOS"
+    };
+    public static final Random RANDOM = new Random();
+    public final int MAX_ERRORS = 7;
+    public String wordToFind;
+    // public char[] wordFound;
+    // public int nbErrors;
+    // public ArrayList<String> letters = new ArrayList<>();
+
+    public ArrayList<Character> playerGuess = new ArrayList<>();
+
 
     private GameEngine(){
 
@@ -19,7 +37,38 @@ public class GameEngine {
         return hangmanGame;
     }
 
-    public static void promptSysout(){
-        System.out.println("Testing just ");
+    public String nextWordToFind() {
+        return WORDS[RANDOM.nextInt(WORDS.length)];
     }
+
+    /*private String wordFoundContent() {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < wordFound.length; i++) {
+            builder.append(wordFound[i]);
+
+            if (i < wordFound.length - 1) {
+                builder.append("");
+            }
+        }
+        return builder.toString();
+    }*/
+
+
+
+    /*public void playerTakesTurn() {
+
+        for (int i = 0; i < wordToFind.length(); i++) {
+
+            if (playerGuess.contains(wordToFind.charAt(i))) {
+                GameActivity.setWordToGuess(wordToFind.charAt(i));
+            }
+            else {
+                GameActivity.setWordToGuess("-");
+            }
+
+        }
+
+    }*/
+
 }
