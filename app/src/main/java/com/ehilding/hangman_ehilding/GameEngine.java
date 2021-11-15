e10f178d4a154482626c2f5b9947c0205338d1e9
ehilding <erik.hildingsson@gmail.com>
2021-11-15 19:19:37 +0100

    Lite design och europas länder i ordlistan

--- app/src/main/java/com/ehilding/hangman_ehilding/GameEngine.java
+++ app/src/main/java/com/ehilding/hangman_ehilding/GameEngine.java
@@ -11,7 +11,12 @@ public class GameEngine {
     private static GameEngine hangmanGame = null;
 
     private static final String[] WORDS = {
-            "AABCDEEF", "GGOIAA"
+            "RYSSLAND", "UKRAINA", "FRANKRIKE", "SPANIEN", "SVERIGE", "NORGE", "TYSKLAND", "FINLAND",
+            "POLEN", "ITALIEN", "STORBRITANNIEN", "RUMÄNIEN", "BELARUS", "GREKLAND", "BULGARIEN", "ISLAND",
+            "UNGERN", "PORTUGAL", "ÖSTERRIKE", "TJECKIEN", "SERBIEN", "IRLAND", "LITAUEN", "LETTLAND",
+            "KROATIEN", "BOSNIEN", "SLOVAKIEN", "ESTLAND", "DANMARK", "NEDERLÄNDERNA", "SCHWEIZ", "MOLDAVIEN",
+            "BELGIEN", "ALBANIEN", "NORDMAKEDONIEN", "TURKIET", "SLOVENIEN", "MONTENEGRO", "KOSOVO",
+            "AZERBAJDZJAN", "GEORGIEN", "LUXEMBURG", "ANDORRA", "MALTA", "LIECHTENSTEIN", "MONACO", "VATIKANSTATEN"
     };
     public static final Random RANDOM = new Random();
     public final int MAX_ERRORS = 7;
