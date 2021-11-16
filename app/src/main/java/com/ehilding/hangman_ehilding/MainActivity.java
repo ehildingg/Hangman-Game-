f69250f1181768db9e09947a8ea97977a0df42af
ehilding <erik.hildingsson@gmail.com>
2021-11-16 15:57:19 +0100

    Design main ok

--- app/src/main/java/com/ehilding/hangman_ehilding/MainActivity.java
+++ app/src/main/java/com/ehilding/hangman_ehilding/MainActivity.java
@@ -6,34 +6,83 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonOne;
-    Button buttonTwo;
+    Button btnPlay, btnAbout;
+    ImageButton btnPlayAction, btnAboutAction;
+    TextView activityNotice;
+    final String activityTitle = "Huvudmeny";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonOne = (Button) findViewById(R.id.button_one);
-        buttonTwo = (Button) findViewById(R.id.button_two);
+         init();
+         initOnClickers();
 
-        buttonOne.setOnClickListener(new View.OnClickListener() {
+    }
+
+    private void init() {
+        btnPlay = (Button) findViewById(R.id.btn_play_game_main);
+        btnAbout = (Button) findViewById(R.id.btn_about_game_main);
+        activityNotice = (TextView) findViewById(R.id.act_location);
+        activityNotice.setText(activityTitle);
+        btnPlayAction = (ImageButton) findViewById(R.id.act_play);
+        btnAboutAction = (ImageButton) findViewById(R.id.act_info);
+
+    }
+
+    private void initOnClickers() {
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, GameActivity.class);
-                startActivity(intent);
+
+                goToPlay();
+
             }
         });
 
-        buttonTwo.setOnClickListener(new View.OnClickListener() {
+        btnAbout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
-                startActivity(intent);
+                goToAbout();
             }
         });
+
+        btnPlayAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToPlay();
+            }
+        });
+
+        btnAboutAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToAbout();
+            }
+        });
+
+
+    }
+
+    private void goToPlay() {
+
+        Intent intent = new Intent(MainActivity.this, GameActivity.class);
+        startActivity(intent);
+
+    }
+
+    private void goToAbout() {
+
+        Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+        startActivity(intent);
+
     }
 }
