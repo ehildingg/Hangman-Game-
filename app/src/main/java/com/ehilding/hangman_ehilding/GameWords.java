ac27eef7b0b09c12df84c2bbcd719fba2ae590fd
ehilding <erik.hildingsson@gmail.com>
2021-11-18 20:59:31 +0100

    Fungerande klar version

--- app/src/main/java/com/ehilding/hangman_ehilding/GameWords.java
+++ app/src/main/java/com/ehilding/hangman_ehilding/GameWords.java
@@ -6,12 +6,12 @@ public class GameWords {
     private static GameWords hangmanGame = null;
 
     private static final String[] WORDS = {
-            "RYS", /*"UKRAINA", "FRANKRIKE", "SPANIEN", "SVERIGE", "NORGE", "TYSKLAND", "FINLAND",*/
-            /*"POLEN", "ITALIEN", "STORBRITANNIEN", "RUMÄNIEN", "BELARUS", "GREKLAND", "BULGARIEN", "ISLAND",
+            "RYSSLAND", "UKRAINA", "FRANKRIKE", "SPANIEN", "SVERIGE", "NORGE", "TYSKLAND", "FINLAND",
+            "POLEN", "ITALIEN", "STORBRITANNIEN", "RUMÄNIEN", "BELARUS", "GREKLAND", "BULGARIEN", "ISLAND",
             "UNGERN", "PORTUGAL", "ÖSTERRIKE", "TJECKIEN", "SERBIEN", "IRLAND", "LITAUEN", "LETTLAND",
             "KROATIEN", "BOSNIEN", "SLOVAKIEN", "ESTLAND", "DANMARK", "NEDERLÄNDERNA", "SCHWEIZ", "MOLDAVIEN",
             "BELGIEN", "ALBANIEN", "NORDMAKEDONIEN", "TURKIET", "SLOVENIEN", "MONTENEGRO", "KOSOVO",
-            "AZERBAJDZJAN", "GEORGIEN", "LUXEMBURG", "ANDORRA", "MALTA", "LIECHTENSTEIN", "MONACO", "VATIKANSTATEN"*/
+            "AZERBAJDZJAN", "GEORGIEN", "LUXEMBURG", "ANDORRA", "MALTA", "LIECHTENSTEIN", "MONACO", "VATIKANSTATEN"
     };
     public static final Random RANDOM = new Random();
     public final int MAX_ERRORS = 7;
